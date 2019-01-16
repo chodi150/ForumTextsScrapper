edb47091a82d93d38a5afcacbaf8fe18ca309074
Piotr Chodyko <piotr.chodyko@gmail.com>
2019-01-16 19:53:28 +0100

    Added file with default values

---
+++ properties/default_values.py
@@ -0,0 +1,2 @@
+glove_window_size = 5
+glove_vector_dimension = 100
--- representation_exporter.py
+++ representation_exporter.py
@@ -1,33 +1,31 @@
 import argparse
 import datetime
 from facades import export_representation_facade
+from properties.default_values import glove_window_size, glove_vector_dimension
 
-
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="Help of export module")
 parser.add_argument('-forum', help='Forum link - necessary to start scraping', required=True)
 parser.add_argument('-day', help='Day', required=True)
 parser.add_argument('-month', help='month', required=True)
 parser.add_argument('-year', help='year', required=True)
-parser.add_argument('-filename', help='year', required=False)
+parser.add_argument('-filename', help='year', required=True)
 parser.add_argument('-mode', help='glove or tfidf', required=True)
-
+parser.add_argument('-glove_window', help='Size of context window in GloVe, default value: 100', required=False)
+parser.add_argument('-glove_vec_dim', help='Used vector dimension in GloVe', required=False)
 
 args = parser.parse_args()
 filter_date = datetime.datetime(int(args.year), int(args.month), int(args.day))
 forum_id = int(args.forum)
 mode = args.mode
-filename = "output" if args.filename is None else args.filename
+filename =  args.filename
 
 current_date = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M")
 filename = filename + current_date + ".csv"
 
-window_size = 5
-vec_dim = 100
-
 if mode == "glove":
-    filename = "window_size_" + str(window_size) + "_vec_dim_" + str(vec_dim) + "_" + filename
+    filename = "window_size_" + str(glove_window_size) + "_vec_dim_" + str(glove_vector_dimension) + "_" + filename
     filename = "glove_" + filename
-    export_representation_facade.do_glove(forum_id, filter_date, filename, window_size = window_size, vec_dim=vec_dim)
+    export_representation_facade.do_glove(forum_id, filter_date, filename, window_size =glove_window_size, vec_dim=glove_vector_dimension)
 elif mode == "tfidf":
     filename = "tfidf_" + filename
     export_representation_facade.do_tfidf(forum_id, filter_date, filename)
