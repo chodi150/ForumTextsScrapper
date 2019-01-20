b9ae9a5d1b7f718faafef2fcbad537441b7b996c
Piotr Chodyko <piotr.chodyko@gmail.com>
2019-01-20 23:12:36 +0100

    + rules corrected

--- properties/mapping.py
+++ properties/mapping.py
@@ -7,7 +7,7 @@ author = "author"
 post_whole = "post_whole"
 post_body = "post_body"
 post_date = "post_date"
-next_page = "next_page"
+next_page = "next_page_whole"
 next_page_link = "next_page_link"
 
 all_mappings = [category_whole, category_title, topic_whole, topic_title, topic_date, author, post_whole,
